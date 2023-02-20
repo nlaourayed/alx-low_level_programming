f0cb5aa877e4bef72d6ab5ebcdb84128d7e8fbdc
“NIZAR <nlaourayed@gmail.com>
2023-02-20 11:44:20 -0800

    ss

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print print combination of 2 digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 48;
+
+	while (a < 58)
+	{
+		b = 48;
+		while (b < 58)
+		{	
+			if (a != b && a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a == 56 && b == 57 )
+				{
+					break;
+				}
+					
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
