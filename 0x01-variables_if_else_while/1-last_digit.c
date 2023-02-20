7bbde0ac87801d03643ee7a3c263157587e3b298
“NIZAR <nlaourayed@gmail.com>
2023-02-20 07:11:42 -0800

    ss

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,10 +12,11 @@ int main(void)
 {
 	int n;
 	int l;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
-	printf("the last digit of %d is %d ", n , l);
+	printf("Last digit of %d is %d ",n ,l);
 	if (l > 5)
 		printf("and is greater than 5\n");
 	else if (l == 0)
