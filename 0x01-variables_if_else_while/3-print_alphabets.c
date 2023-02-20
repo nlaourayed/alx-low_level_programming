cb5388abde6c7f7e7d222ba47f135887fea19773
“NIZAR <nlaourayed@gmail.com>
2023-02-20 07:55:49 -0800

    ss

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	for (a = 'A'; a<= 'Z'; a++)
+		putchar(a);
+
+	putchar('\n');
+	return (0);
+}
