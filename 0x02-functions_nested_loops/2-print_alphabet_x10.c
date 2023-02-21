8edc02807ccd0fb01b3eac97f3e236919e727b52
“NIZAR <nlaourayed@gmail.com>
2023-02-21 14:22:08 -0800

    ss

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10  - Print Alphabet 10 tinmes
+ * function that prints alphabets 10 times
+ * Return: Always 0 (Succsses)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	for(i = 0; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+}
