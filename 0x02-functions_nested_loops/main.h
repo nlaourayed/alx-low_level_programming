a64fe8c5346495be657f8a9102672e82c8b20678
“NIZAR <nlaourayed@gmail.com>
2023-02-21 13:58:01 -0800

    ss

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,1 @@
-/**
- *
- * Main Header File
- *
- */
-#include <unistd.h>
-int _putchar(char c);
+
---
+++ 0x02-functions_nested_loops/mainb.h
@@ -0,0 +1,7 @@
+/**
+ *
+ * Main Header File
+ *
+ */
+#include <unistd.h>
+int _putchar(char c);
