2a096458dd94a9b523cd339ce4e066c5d8cc552b
“NIZAR <nlaourayed@gmail.com>
2023-02-21 15:20:57 -0800

    ss

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,14 @@
 /**
  * _islower - Is LowerCase
  * check if the character is an lowercase alphabet
+ * @c: is the character to be checked
+ *
  * Return: 1 if true and 0 otherwise
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-		return(1);
-	else 
-		return(0);	
+		return (1);
+	else
+		return (0);	
 }
