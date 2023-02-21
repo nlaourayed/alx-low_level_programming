37088ee52028b6e2417f671aa5dd0f570ca22aa8
“NIZAR <nlaourayed@gmail.com>
2023-02-21 12:35:56 -0800

    ss

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "_putchar.h"
+
+/**
+ * 
+ * Print From _putchar
+ *
+ * Always Return 0
+ */
+
+int main (void)
+{
+	_putchar('a');
+	_putchar('a');
+	_putchar('a');
+	_putchar('\n');
+	return (0);
+}
