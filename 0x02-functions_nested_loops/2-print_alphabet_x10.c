d885b849de3446dfe377854c727f78431fd56276
“NIZAR <nlaourayed@gmail.com>
2023-02-21 14:23:25 -0800

    ss

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,6 +6,18 @@
  * Return: Always 0 (Succsses)
  */
 
+void print_alphabet(void)
+{
+        int a = 97;
+
+        while (a <= 122)
+        {
+                _putchar(a);
+                a++;
+        }
+        _putchar('\n');
+}
+
 void print_alphabet_x10(void)
 {
 	int i = 0;
