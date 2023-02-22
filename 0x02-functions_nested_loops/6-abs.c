0d3e8329ec5f566aa66a3113a84e9882d60b3d76
“NIZAR <nlaourayed@gmail.com>
2023-02-21 16:46:57 -0800

    ss

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - abs of number
+ * calculate the absolute value of integer
+ * @i: the intiger
+ *
+ * Return: positive number;
+ */
+int _abs(int i)
+{
+	int abs = 0;
+
+	if (i < 0)
+		abs = i * -1;
+	return (i);
+}
