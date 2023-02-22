20d839be9391e8414a8cf5a3a9edb3ae8eba1ccb
“NIZAR <nlaourayed@gmail.com>
2023-02-21 16:54:08 -0800

    ss

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Print It
+ * print the last digit of the number
+ * @n: the number
+ *
+ * Return: the last digit of n
+ */
+int print_last_digit(int n){
+	int last_digit;
+
+	last_digit = n % 10;
+	return (last_digit);
+}
