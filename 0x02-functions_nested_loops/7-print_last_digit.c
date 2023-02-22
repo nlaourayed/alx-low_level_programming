77b75694190391a49856a3f31018be3ac13b1da0
“NIZAR <nlaourayed@gmail.com>
2023-02-21 17:23:28 -0800

    ss

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,8 +10,7 @@
 int print_last_digit(int n){
 	int last_digit;
 
-	last_digit = 0;
 	last_digit = n % 10;
-	_putchar(last_digit);
-	return ('0' + last_digit);
+	_putchar(last_digit + 48);
+	return (last_digit);
 }
