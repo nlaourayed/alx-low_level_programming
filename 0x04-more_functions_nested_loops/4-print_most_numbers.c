a4a3a3f6b4c690f835730704e989a1a6ee59e7fc
“NIZAR <nlaourayed@gmail.com>
2023-02-24 15:26:18 -0800

    ss

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - print numbers from 0 to 9 except 2, 4
+ * print_most_numbers - print numbers from 0 to 9 except 2, 4
  *
  * Return: Always return 0
  */
@@ -12,10 +12,8 @@ void print_most_numbers(void)
 	while (i <= 57)
 	{
 		if (i != 50 || i != 52)
-		{
 			_putchar(i);
-			i++;
-		}
+		i++;
 	}
 	_putchar('\n');
 }
