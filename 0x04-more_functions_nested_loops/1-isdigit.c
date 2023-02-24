e2ef97aa1743a02e23d97b671f42e4d2456c5dc7
“NIZAR <nlaourayed@gmail.com>
2023-02-24 13:16:57 -0800

    ss

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _isdigit- check if the char is digital numbeer from 0 to 9
+ * _isdigit - check if the char is digital numbeer from 0 to 9
  * @c: theint
  *
  * Return: 0 if c is a digit, 0 otherwise
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c <= 48 && c >= 57)
 		return (1);
