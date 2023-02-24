414486c563ad0b8fd19e7190a3e21dba4fe26b53
“NIZAR <nlaourayed@gmail.com>
2023-02-24 14:46:00 -0800

    ss

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multipli 2 integer
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
