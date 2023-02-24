b59ea47eba351b09c94dfa9d25a93947700918f1
“NIZAR <nlaourayed@gmail.com>
2023-02-24 15:59:22 -0800

    ss

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers from 0 to 14 10 times
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	more_numbers();
+}
+void more_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 14)
+	{
+		_putchar((i / 10) + 48);
+		_putchar((i % 10) + 48);
+		_putchar('\n');
+		i++;
+	}
+}
