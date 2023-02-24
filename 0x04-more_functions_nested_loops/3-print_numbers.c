959492c4d4ca26424d9c1feab1d953d22b38d17c
“NIZAR <nlaourayed@gmail.com>
2023-02-24 14:52:49 -0800

    ss

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ */
+int print_numbers()
+{
+	int i;
+	
+	while (i <= 48 + 9)
+	{
+		_putchar(i);
+		i++;
+	}
+	return (0);
+}
