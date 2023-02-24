0a0e7b7221962806cc57cb97e57a00f71f8dd370
“NIZAR <nlaourayed@gmail.com>
2023-02-24 12:53:43 -0800

    ss

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - check if the char is uppercase
+ * @c: the char
+ *
+ * Return: 0 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c <= 90 && >= 65)
+		return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+/**
+ *
+ * Main Header File
+ *
+ */
+#include <unistd.h>
+int _putchar(char c);
+int _isupper(int c);
